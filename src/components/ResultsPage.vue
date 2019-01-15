80dfaf32d5be5c454f4f4e28bb83b8197ebe5c45
Sladjana <sladjana.pajich@gmail.com>
2019-01-15 19:21:13 +0100

    result & voting component added

---
+++ src/components/ResultsPage.vue
@@ -0,0 +1,22 @@
+<template>
+<div>results page</div>
+</template>
+
+<script>
+export default {
+  name: 'ResultsPage',
+  components: { },
+  mounted () {
+  },
+  methods: {
+  },
+  computed: {
+  },
+  watch: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/VotingPage.vue
@@ -0,0 +1,22 @@
+<template>
+<div>vote page</div>
+</template>
+
+<script>
+export default {
+  name: 'VotingPage',
+  components: { },
+  mounted () {
+  },
+  methods: {
+  },
+  computed: {
+  },
+  watch: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
